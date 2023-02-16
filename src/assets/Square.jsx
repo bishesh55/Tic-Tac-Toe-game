86a22a8fef6742bb6a8e2323423a2021cdd9c16e
bishesh <biahesh.sthapit@gmail.com>
2023-02-16 16:26:08 +0545

    more changes

--- src/assets/Square.jsx
+++ src/assets/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Square(props) {
   return (
     <div onClick={props.onClick} className='box-square'>
-      <h4>{props.state}</h4>
+      <h4 className='font-xl'>{props.state}</h4>
     </div>
   )
 }
